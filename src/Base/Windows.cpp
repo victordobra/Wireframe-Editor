a1c1656c6b90c8a24a7a82a919e9129387d1e61a
Victor Dobra <victordobra327@gmail.com>
2022-07-26 13:34:47 +0300

    Added dock space

--- src/Base/Windows.cpp
+++ src/Base/Windows.cpp
@@ -13,9 +13,29 @@ namespace wfe::editor {
 
     }
     void Window::RenderWindows() {
+        // Begin the frame
         ImGui::NewFrame();
+
+        // Create the dock space
+        ImGui::SetNextWindowPos(ImGui::GetMainViewport()->WorkPos);
+        ImGui::SetNextWindowSize(ImGui::GetMainViewport()->WorkSize);
+        ImGui::SetNextWindowViewport(ImGui::GetMainViewport()->ID);
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+
+        ImGuiWindowFlags dockSpaceFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        ImGui::Begin("DockSpace", nullptr, dockSpaceFlags);
+
+        ImGui::PopStyleVar(2);
+
+        ImGuiID dockSpaceID = ImGui::GetID("DockSpace");
+        ImGui::DockSpace(dockSpaceID, ImVec2(0.f, 0.f), ImGuiDockNodeFlags_None);
+
         for(auto* window : windows)
             window->RenderWindow();
+
+        ImGui::End();
         ImGui::Render();
     }
 
